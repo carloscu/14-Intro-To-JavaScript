021a8d92ca03969af76cdd1664831ea7f3a44011
carloscu <carloscu@me.com>
2019-03-11 22:17:28 -0400

    updating the app js

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+
+data.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.defineProperties(ufoReport.forEach(([key, value]) => {
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
+
+
 // YOUR CODE HERE!
