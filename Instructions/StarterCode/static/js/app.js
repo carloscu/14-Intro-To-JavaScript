d9328be22df5647419d8d8497af35b1c193c99b8
carloscu <carloscu@me.com>
2019-03-13 18:35:50 -0400

    updating app.js

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,15 +1,25 @@
 // from data.js
-var tableData = data;
 
-var tbody = d3.select("tbody");
+function showTable (dataset){
 
-data.forEach((ufoReport) => {
-    var row = tbody.append("tr");
-    Object.defineProperties(ufoReport.forEach(([key, value]) => {
-        var cell = tbody.append("td");
-        cell.text(value);
-    });
-});
-
-
-// YOUR CODE HERE!
+    var data = dataset;
+    //}
+    var tbody = d3.select("tbody");
+    
+    
+    // YOUR CODE HERE!
+    
+    data.forEach((ufoReport) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key, value]) => {
+          var cell = tbody.append("td");
+          cell.text(value);
+        });
+      });
+    };
+    
+    showTable(data);
+    
+    // YOUR CODE HERE!
+    
+    
